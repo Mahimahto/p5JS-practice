5043dedd5c7060d2f36ac42815999ceb652b6e9e
Manisha Kumari <manisha.tkg08@gmail.com>
2024-12-03 11:36:19 +0530

    Implemented p5.js canvas with interactive ellipse; added comments to explain setup and draw functions

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// setup() function ek baar run hota hai jab sketch start hota hai
+function setup() {
+    // Canvas (drawing area) create karna jiska size 400px by 400px hai
+    createCanvas(400, 400);
+  }
+  
+  // draw() function har frame par baar-baar run hota hai (default: 60 frames per second)
+  function draw() {
+    // Background ka color set karna (220 ek light gray shade hai)
+    background(220);
+  
+    // Mouse ke current position (mouseX, mouseY) par ek circle draw karna
+    // ellipse(centerX, centerY, width, height)
+    ellipse(mouseX, mouseY, 50, 50); // Circle ka width aur height 50px hai
+  }
+  
